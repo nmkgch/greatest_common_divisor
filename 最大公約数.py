1fe903b3185090387a2c9e89eea5289e9c8a0511
nmshrkgch <nmshrkgch@gmail.com>
2019-09-27 11:26:25 +0900

    文言を変更

--- 最大公約数.py
+++ 最大公約数.py
@@ -1,7 +1,7 @@
 cnt=[]
-kaisu=(int(input('対象の数値の数を入れてください：')))+1
+kaisu=(int(input('対象の数値の数を入力してください：')))+1
 for ji in range(1,kaisu):
-    cnt.append(int(input(str(ji)+'個目の数値を入れてください：')))
+    cnt.append(int(input(str(ji)+'個目の数値を入力してください：')))
 cnt1=max(cnt)
 cnt.remove(cnt1)
 cnt2=max(cnt)
